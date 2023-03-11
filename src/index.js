d48dcb7e2f59fccccbd9ff71bc31a17cc4810ab6
George <81578359+georgy-p@users.noreply.github.com>
2023-03-11 16:10:51 +0300

    import bootstrap and custom styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/style.css';
 import App from './App';
-import 'antd/dist/reset.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
