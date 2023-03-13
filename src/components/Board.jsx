6550e873cf054c12c8962605ad6ec4a2957101d2
George <81578359+georgy-p@users.noreply.github.com>
2023-03-13 11:59:53 +0300

    add Droppable component

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
+import { Droppable } from 'react-beautiful-dnd';
+// import { useSelector } from 'react-redux';
 import { ReactComponent as AddLogo } from '../img/add.svg';
 
-const Board = () => {
-  return (
-    <div className="rounded board-outer-container">
-          <div className="board-inner-container">
-              <AddLogo />
-              <p className="board-text-highlight" >Перетащите сюда</p>
-              <div className="board-text">
-                <p>любой элемент из левой панели</p>
-              </div>
-          </div>
-    </div>
+// For the first div on snapshot
+// style={{ background: '#F0F9FF' }}
 
+const Board = () => {
+  // const elements = useSelector((state) => state.constructor.elements);
+  // console.log(elements);
+
+
+  return (
+    <Droppable droppableId="board">
+      {(provided) => (
+        <div 
+          ref={provided.innerRef}
+            
+          className="rounded board-outer-container"
+          {...provided.droppableProps}
+          >
+            { (
+              <div className="board-inner-container">
+                <AddLogo />
+                <p className="board-text-highlight" >Перетащите сюда</p>
+                <div className="board-text">
+                  <p>любой элемент из левой панели</p>
+                </div>
+              </div>
+            )}
+            {provided.placeholder}
+      </div>
+      )}
+    </Droppable>
   )
 }
 
