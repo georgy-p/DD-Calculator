3e59a002f8c3a4fdbfd1e5ebb73d4b6fcd6a6043
George <81578359+georgy-p@users.noreply.github.com>
2023-03-13 12:04:28 +0300

    add DragDropContext

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { DragDropContext } from 'react-beautiful-dnd';
 import { Col, Row } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 import Board from './Board';
 import Display from './calculator_parts/Display';
 import Equal from './calculator_parts/Equal';
@@ -10,26 +12,36 @@ import SwitchBtns from './SwitchBtns';
 
 
 const MainContainer = () => {
+  const isRuntime = useSelector((state) => state.constructor.isRuntime);
+
+  const onDragEnd = useCallback(() => {
+    //
+  }, [])
+
   return (
     <div className="main-content-container">
-      <Row>
-        <Col>
-          <div className="elements-container">
-            <Display />
-            <Operators />
-            <Numbers />
-            <Equal />
-          </div>
-        </Col>
-        <Col>
-          <div className="mode-container">
-            <SwitchBtns />
-            <Board />
-          </div>
-        </Col>
-      </Row>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Row>
+          <Col>
+            {!isRuntime &&  
+            <div className="elements-container">
+              <Display />
+              <Operators />
+              <Numbers />
+              <Equal />
+            </div>
+            }
+          </Col>
+          <Col>
+            <div className="mode-container">
+              <SwitchBtns />
+              <Board />
+            </div>
+          </Col>
+        </Row>
+      </DragDropContext>
     </div>
   )
 }
 
-export default MainContainer
+export default MainContainer;
