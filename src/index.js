eb904ce5fd516f607da549be4fa111a58b46b107
George <81578359+georgy-p@users.noreply.github.com>
2023-03-13 12:00:37 +0300

    add provider and connect store

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/style.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './slices/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
 
