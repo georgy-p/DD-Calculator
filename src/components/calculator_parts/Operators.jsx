702e3418f930bee0328f65269e39d7e1ed31d445
George <81578359+georgy-p@users.noreply.github.com>
2023-03-13 12:07:16 +0300

    add click handlers

--- src/components/calculator_parts/Operators.jsx
+++ src/components/calculator_parts/Operators.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import { Button, Col, Row } from 'react-bootstrap'
+import { useDispatch } from 'react-redux';
+import { addOperator } from '../../slices/calculatorSlice';
 
 const Operators = () => {
+  const operatorBtns = ['/', '*', '-', '+'];
+  const dispatch = useDispatch();
+  
+  const handleClick = (operator) => dispatch(addOperator(operator));
+
   return (
-    <Row style={{ height: '56px'}}
+    <Row key="operators" style={{ height: '56px'}}
       className="shadow mt-4 rounded d-flex align-content-center justify-content-center c-outer-width">
       <div className="gx-2 operators-inner-container c-inner-width">
-        <Col>
-              <Button className='border btn-operators' variant='light'>/</Button>
-        </Col>
-        <Col>
-              <Button  className='border btn-operators' variant='light'>x</Button>
-        </Col>
-        <Col>
-              <Button  className='border btn-operators' variant='light' >-</Button>
-        </Col>
-        <Col>
-              <Button className='border btn-operators' variant='light'>+</Button>
-        </Col>
+        {operatorBtns.map((opBtn) => {
+          return (
+            <Col key={opBtn}>
+              <Button 
+                onClick={() => handleClick(opBtn)}
+                className='border btn-operators' 
+                variant='light' 
+                value={opBtn}>{opBtn}</Button>
+            </Col>
+          )
+        })}
       </div>
     </Row>
   )
