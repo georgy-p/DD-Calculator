71e1358d3b1ffb9f0c82c51a9ea71950d539ae43
George <81578359+georgy-p@users.noreply.github.com>
2023-03-13 12:05:02 +0300

    add onClick handler

--- src/components/SwitchBtns.jsx
+++ src/components/SwitchBtns.jsx
@@ -2,20 +2,29 @@ import React from 'react';
 import { Col, Row, ToggleButton, ToggleButtonGroup } from 'react-bootstrap';
 import { ReactComponent as Eyelogo} from '../img/eye-outline.svg';
 import { ReactComponent as  Codelogo } from '../img/code-outline.svg';
+import { useDispatch } from 'react-redux';
+import { offRuntime, onRuntime } from '../slices/constructorSlice';
 
 
 
 const SwitchBtns = () => {
+  const dispatch = useDispatch();
+  
+  // Намеренно пока не вызываю функцию, потому что возникает ошибка редьюсера
+  const handleOn = () => dispatch(onRuntime);
+  const handleOff = () => dispatch(offRuntime);
+
+
   return (
     <div className="switch-btns">
       <ToggleButtonGroup type="radio" defaultValue={2} name="options">
-          <ToggleButton id="tbg-1" variant="light" className="btn-runtime" value={1}>
+          <ToggleButton onClick={handleOn} id="tbg-1" variant="light" className="btn-runtime" value={1}>
             <Row>
               <Col className='col-1'><Eyelogo /></Col>
               <Col className="btn-text">Runtime</Col>
             </Row>
           </ToggleButton>
-          <ToggleButton id="tbg-2" variant="light" className="btn-constructor" value={2}>
+          <ToggleButton onClick={handleOff} id="tbg-2" variant="light" className="btn-constructor" value={2}>
             <Row>
               <Col className='col-1'><Codelogo /></Col>
               <Col className="btn-text">Constructor</Col>
