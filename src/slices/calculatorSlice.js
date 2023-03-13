cb283fab34c8776368a83f8db91804206fa56897
George <81578359+georgy-p@users.noreply.github.com>
2023-03-13 12:01:22 +0300

    add calculator reducer

---
+++ src/slices/calculatorSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  nums: {
+    num1: [0],
+    num2: [],
+  },
+  operator: null,
+};
+
+export const calculatorSlice = createSlice({
+  name: 'calculator',
+  initialState,
+  reducers: {
+    addNum1: (state, { payload }) => {
+      state.nums.num1.push(payload);
+    },
+    addNum2: (state, { payload }) => {
+      state.nums.num2.push(payload);
+    },
+    addOperator: (state, { payload }) => {
+      state.operator = payload;
+    }
+  }
+});
+
+export const { addNum1, addNum2, addOperator } = calculatorSlice.actions;
+export default calculatorSlice.reducer;
+
