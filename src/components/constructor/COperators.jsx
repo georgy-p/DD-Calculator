ea845e20a68d726ba826b9bea413a6bc92c6776a
George <81578359+georgy-p@users.noreply.github.com>
2023-03-17 17:48:21 +0300

    change mt from mt-4 to mt-2 from Row

--- src/components/constructor/COperators.jsx
+++ src/components/constructor/COperators.jsx
@@ -7,7 +7,7 @@ const COperators = () => {
   return (
           <Row 
             key="operators" 
-            className="shadow mt-4 rounded operators-outer-container c-outer-width">
+            className="shadow mt-2 rounded operators-outer-container c-outer-width">
           <div className="gx-0 operators-inner-container c-inner-width">
             {operatorBtns.map((opBtn) => {
               return (
