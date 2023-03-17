4fc7156ca4477d06aa64f40401b78628607dbafb
George <81578359+georgy-p@users.noreply.github.com>
2023-03-17 17:42:13 +0300

    add runtime render

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,15 +4,18 @@ import { ReactComponent as AddLogo } from '../img/add.svg';
 import { useDrop } from 'react-dnd'
 import { ItemTypes } from '../itemTypes';
 import { constructorComponents } from './constructor';
-// style={{ background: '#F0F9FF' }}
+import Calculator from './Calculator';
 
 const Board = () => {
   const elements = useSelector((state) => state.ui.elements.board);
+  const isRuntime = useSelector((state) => state.ui.isRuntime);
+
   const renderElements = () => (
-    <div style={{display: 'flex', flexDirection: 'column', border: 'none', alignItems: 'center' }}>
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       {elements.map((el) => <div key={el}>{constructorComponents[el]()}</div>)}
     </div>
   )
+
   const renderBoard = () => (
     <div className="board-inner-container">
         <AddLogo />
@@ -23,7 +26,20 @@ const Board = () => {
     </div>
   );
 
-  const containerRender = elements.length > 0 ? renderElements() : renderBoard();
+  let border = elements.length > 0 ? 'none' : '2px dashed #C4C4C4';
+  let display = elements.length  > 0 ? null : 'flex';
+
+  const containerRender = () => {
+    if (elements.length > 0) {
+      if (isRuntime) {
+        return (<Calculator />)
+      } 
+        return renderElements();
+    } else {
+      return renderBoard();
+    }
+  }
+
   
   const [{ canDrop, isOver }, drop] = useDrop(() => ({
     accept: ItemTypes.BOX,
@@ -35,14 +51,15 @@ const Board = () => {
   }));
 
   const isActive = canDrop && isOver;
-
+  
   let backgroundColor = isActive ? '#F0F9FF' : '#fff';
 
+
   return (
-        <div ref={drop} style={{ backgroundColor }}
+        <div ref={drop} style={{ backgroundColor, border, display }}
           className="rounded board-outer-container"
           >
-          {containerRender}
+          {containerRender()}
       </div>
   )
 }
