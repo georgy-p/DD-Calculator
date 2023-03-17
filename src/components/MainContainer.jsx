47cd61a5c1204f978c35afa8f3c8a584aa20ee7c
George <81578359+georgy-p@users.noreply.github.com>
2023-03-17 17:43:03 +0300

    add runtime render logic

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -9,31 +9,38 @@ import CNumbers from './constructor/CNumbers';
 import COperators from './constructor/COperators';
 import SwitchBtns from './SwitchBtns';
 
-const renderDrag = (element, name) => {
-  const isDroped = useSelector((state) => state.ui.elements.mainContainer[name]);
-  const dropStyle = { pointerEvents: 'none', opacity: 0.4 };
-  if (isDroped) {
-    return <div style={dropStyle}>{element}</div>
-  } else {
-    return <Box name={name}>{element}</Box>;
-  }
-}
+
 
 const MainContainer = () => {
-  // const isRuntime = useSelector((state) => state.ui.isRuntime);
-  // console.log(isRuntime);
+  const isRuntime = useSelector((state) => state.ui.isRuntime);
+  const droppedItems = useSelector((state) => state.ui.elements.mainContainer);
 
+  const renderDrag = (element, name) => {
+    const dropStyle = { pointerEvents: 'none', opacity: 0.4 };
+    const isDroped = droppedItems[name];
+    if (isDroped) {
+      return <div style={dropStyle}>{element}</div>
+    } else {
+      return <Box name={name}>{element}</Box>;
+    }
+  }
+
+  const renderCol = () => (
+        <div className="elements-container ">
+            {renderDrag(<CDisplay />, 'display')}
+            {renderDrag(<COperators />, 'operators')}
+            {renderDrag(<CNumbers />, 'numbers')}
+            {renderDrag(<CEqual />, 'equal')}
+        </div>
+  )
+
+  const isHide = isRuntime ? null : renderCol();
+  
   return (
     <div className="main-content-container">
         <Row>
           <Col> 
-                <div                   
-                  className="elements-container ">
-                    {renderDrag(<CDisplay />, 'display')}
-                    {renderDrag(<COperators />, 'operators')}
-                    {renderDrag(<CNumbers />, 'numbers')}
-                    {renderDrag(<CEqual />, 'equal')}
-                </div>
+               {isHide}
           </Col>
           <Col>
             <div className="mode-container">
