b239b30eddb248a341511fbea241befe719f363b
George <81578359+georgy-p@users.noreply.github.com>
2023-03-17 10:59:51 +0300

    fix file

--- src/components/SwitchBtns.jsx
+++ src/components/SwitchBtns.jsx
@@ -3,20 +3,24 @@ import { Col, Row, ToggleButton, ToggleButtonGroup } from 'react-bootstrap';
 import { ReactComponent as Eyelogo} from '../img/eye-outline.svg';
 import { ReactComponent as  Codelogo } from '../img/code-outline.svg';
 import { useDispatch } from 'react-redux';
-import { offRuntime, onRuntime } from '../slices/constructorSlice';
+import { offRuntime, onRuntime } from '../slices/uiSlice';
+import { useSelector } from 'react-redux';
 
 
 
 const SwitchBtns = () => {
   const dispatch = useDispatch();
+  const elements = useSelector((state) => state.ui.elements);
   
   // Намеренно пока не вызываю функцию, потому что возникает ошибка редьюсера
-  const handleOn = () => dispatch(onRuntime);
-  const handleOff = () => dispatch(offRuntime);
+  const handleOn = () => dispatch(onRuntime());
+  const handleOff = () => dispatch(offRuntime());
+  const nonClickable = { pointerEvents: 'none' };
+  const isCompleteConstructor = elements && elements.length === 4 ? null : nonClickable;
 
 
   return (
-    <div className="switch-btns">
+    <div style={isCompleteConstructor} className="switch-btns">
       <ToggleButtonGroup type="radio" defaultValue={2} name="options">
           <ToggleButton onClick={handleOn} id="tbg-1" variant="light" className="btn-runtime" value={1}>
             <Row>
