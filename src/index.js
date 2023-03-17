22aa746a4a208bf33587ad7de8efc0c34d67f164
George <81578359+georgy-p@users.noreply.github.com>
2023-03-17 10:56:48 +0300

    add dnd provider

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/style.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { store } from './slices/store';
+import store from './slices/store';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
-        <App />
+        <DndProvider backend={HTML5Backend}>
+            <App />
+        </DndProvider>
     </Provider>
 );
 
