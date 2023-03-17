440f44375875fc6d074ee4e4776e561cc2752643
George <81578359+georgy-p@users.noreply.github.com>
2023-03-17 10:58:22 +0300

    add renderElemets logic

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,38 +1,49 @@
 import React from 'react';
-import { Droppable } from 'react-beautiful-dnd';
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { ReactComponent as AddLogo } from '../img/add.svg';
-
-// For the first div on snapshot
+import { useDrop } from 'react-dnd'
+import { ItemTypes } from '../itemTypes';
+import { constructorComponents } from './constructor';
 // style={{ background: '#F0F9FF' }}
 
 const Board = () => {
-  // const elements = useSelector((state) => state.constructor.elements);
-  // console.log(elements);
+  const elements = useSelector((state) => state.ui.elements.board);
+  const renderElements = () => (
+    <div style={{display: 'flex', flexDirection: 'column', border: 'none', alignItems: 'center' }}>
+      {elements.map((el) => <div key={el}>{constructorComponents[el]()}</div>)}
+    </div>
+  )
+  const renderBoard = () => (
+    <div className="board-inner-container">
+        <AddLogo />
+        <p className="board-text-highlight" >Перетащите сюда</p>
+        <div className="board-text">
+          <p>любой элемент из левой панели</p>
+        </div>
+    </div>
+  );
 
+  const containerRender = elements.length > 0 ? renderElements() : renderBoard();
+  
+  const [{ canDrop, isOver }, drop] = useDrop(() => ({
+    accept: ItemTypes.BOX,
+    drop: () => ({ name: 'Board' }),
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+      canDrop: monitor.canDrop(),
+    }),
+  }));
+
+  const isActive = canDrop && isOver;
+
+  let backgroundColor = isActive ? '#F0F9FF' : '#fff';
 
   return (
-    <Droppable droppableId="board">
-      {(provided) => (
-        <div 
-          ref={provided.innerRef}
-            
+        <div ref={drop} style={{ backgroundColor }}
           className="rounded board-outer-container"
-          {...provided.droppableProps}
           >
-            { (
-              <div className="board-inner-container">
-                <AddLogo />
-                <p className="board-text-highlight" >Перетащите сюда</p>
-                <div className="board-text">
-                  <p>любой элемент из левой панели</p>
-                </div>
-              </div>
-            )}
-            {provided.placeholder}
+          {containerRender}
       </div>
-      )}
-    </Droppable>
   )
 }
 
